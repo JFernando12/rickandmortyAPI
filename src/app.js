8a2a3f31d560a04fdf167c96b915ed3b8d51eb45
JFernando12 <fire.jf2012@gmail.com>
2022-03-29 13:06:31 -0600

    Avanzando en el proyecto

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan =require("morgan");
+const path = require("path");
+
+const app = express();
+
+//Setting
+
+app.set("port", 3000);
+app.set("AppName", "Rick and Morty API");
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//MiddleWare
+app.use(morgan("dev"));
+app.use(express.json());
+
+//Routes
+app.use(require("./routes/index"));
+
+//Statics
+app.use(express.static("public"));
+
+app.listen(app.get("port"), () => {
+    console.log("Server on port 3000");
+})
