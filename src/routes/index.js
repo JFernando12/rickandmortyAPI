9492862c1f308a90904a442c5f3bd4645d2f5afc
JFernando12 <fire.jf2012@gmail.com>
2022-04-07 17:37:21 -0500

    Terminada la primera etapa

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,41 @@
 const { Router } = require("express");
+const { route } = require("express/lib/application");
 const router = Router();
 const fetchData = require("../utils/fetchData");
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-const API = "https://rickandmortyapi.com/api/character/"
+const rickandmortyAPI = "https://rickandmortyapi.com/api/character/";
+const pokemonAPI = "https://pokeapi.co/api/v2/pokemon/"
 
 router.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("pages/cover.ejs");
 })
 
-router.get("/personaje", async(req, res) => {
-    const data = await(fetchData(API));
-    res.render("pages/characters.ejs", {data: data.results});
+router.get("/rickandmorty", async(req, res) => {
+    const data = await(fetchData(rickandmortyAPI));
+    console.log(data)
+    res.render("pages/characters.ejs", {data: data.results, title: "Rick and Morty"});
+})
+
+router.get("/pokemon", async(req, res) => {
+    const data = await(fetchData(pokemonAPI));
+    console.log(data);
+    const count = data.results.length;
+    console.log(count)
+    let results = [];
+    for (let i=0; i < count; i++) {
+        let namePokemon = data.results[i].name;
+        let img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (i+1) +".png"
+    
+        let pokemon = {
+            name: namePokemon,
+            species: "no hay",
+            status: "alive",
+            image : img
+        }
+        results.push(pokemon);
+    }
+    res.render("pages/characters.ejs", {data: results, title: "Pokemon"});
 })
 
 
